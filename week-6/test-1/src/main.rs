92569156374981bb8675e207b8498e899f2436fa
c.udeh <chimamandau13@gmail.com>
2023-11-19 20:05:29 -0800

    (my commit)

---
+++ week-6/test-1/src/main.rs
@@ -0,0 +1,76 @@
+//Rust program to build a system for OPIS
+
+use std::io;
+
+fn main() {
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    let mut input3 = String::new();
+    let mut input4 = String::new();
+    let mut input5 = String::new();
+    let mut input6 = String::new();
+    let mut input7 = String::new();
+    let mut input8 = String::new();
+
+    println!("Enter your name: ");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+    let name:f32 = input1.trim().parse().expect("Not a valid number");
+
+    println!("Enter date of birth: ");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+    let date : birth::f32 = input2.trim().parse().expect("Not a valid number");
+
+    println!("Enter email address: ");
+    io::stdin().read_line(&mut input3).expect("Not a valid string");
+    let email address:f32 = input3.trim().parse().expect("Not a valid number");
+
+    println!("Enter phone number: ");
+    io::stdin().read_line(&mut input4).expect("Not a valid string");
+    let phone number:f32 = imput4.trim().parse().expect("Not a valid number");
+
+    println!("Enter number of children: ");
+    io::stdin().read_line(&mut input5).expect("Not a valid string");
+    let number of children:f32 = imput5.trim().parse().expect("Not a valid number");
+
+    println!("Enter number of siblings: ");
+    io::stdin().read_line(&mut input6).expect("Not a valid string");
+    let number of siblings:f32 = input6.trim().parse().expect("Not a valid number");
+
+    println!("Enter medical diagnosis: ");
+    io::stdin().read_line(&mut input7).expect("Not a valid string");
+    let medical diagnosis:f32 = input7.trim().parse().expect("Not a valid number")
+
+    println!("Enter village of residence: ");
+    io::stdin().read_line(&mut input8).expect("Not a valid string");
+    let village of residence:f32 = input8.trim().parse().expect("Not a valid number");
+
+    if medical diagnosis > 50 ;
+    if children = 4;
+    if village of residence = Akpabom;me
+    let discount = 20% * 1200000;
+
+    if medical diagnosis = 30;
+    if siblings = 4;
+    if village of residence = Ngbauji;
+    let discount = 5% * 550000
+
+    if medical diagnosis > 40;
+    if siblings > 3;
+    if children > 3;
+    if village of residence = Atabrikang;
+    let discount = 15% * 1500000;
+
+    if medical diagnosis > 28 < 45;
+    if children are > 2 < 4 ;
+    if village of residence = Okorobilom;
+    let discount = 10% * 800000;
+
+    if medical diagnosis > 58;
+    if children > 5;
+    if siblings > 5;
+    if village of residence = Emeremen;
+    let dicount = 10% * 450000;
+
+
+
+}
---
+++ week-6/test-1/target/debug/deps/test_1.d
@@ -0,0 +1,5 @@
+C:\Users\USER\Documents\c.udeh\week-6\test-1\target\debug\deps\test_1.exe: src\main.rs
+
+C:\Users\USER\Documents\c.udeh\week-6\test-1\target\debug\deps\test_1.d: src\main.rs
+
+src\main.rs:
