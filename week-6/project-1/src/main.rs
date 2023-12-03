5b5b3fa3b01a92928ff53f64a850070a218fd85a
c.udeh <chimamandau13@gmail.com>
2023-12-03 13:38:03 -0800

    (my sixth commit)

--- week-6/project-1/src/main.rs
+++ week-6/project-1/src/main.rs
@@ -1,42 +1,68 @@
+
 use std::io;
 
+
+
  fn main() {
+
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    let mut input3 = String::new();
+    let mut input4 = String::new();
+    let mut input5 = String::new();
+    let mut input6 = String::new();
+    let mut input7 = String::new();
+
+
     println!("Enter your name: ");
     io::stdin().read_line(&mut input1).expect("error");
     let name:String = input1.trim().parse().expect("invalid");
 
     println!("Enter email: ");
-    io::stdin().read_line(&mut input2)expect("error");
+    io::stdin().read_line(&mut input2).expect("error");
     let email:String = input2.trim().parse().expect("invalid");
 
     println!("Department: ");
-    io::stdin().read_line(&mut input3)expect("error");
+    io::stdin().read_line(&mut input3).expect("error");
     let department:String = input3.trim().parse().expect("invalid");
 
     println!("State of origin: ");
-    io::stdin().read_line(&mut input4)expect("error");
-    let state of origin:String = input4.trim().parse().expect("invalid");
+    io::stdin().read_line(&mut input4).expect("error");
+    let state_of_origin:String = input4.trim().parse().expect("invalid");
 
-    println!("Are you a class rep?: ");
-    io::stdin().read_line(&mut input5)expect("error");
-    let Are you a class rep:String = input5.trim().parse().expect("invalid");
+    println!("Are you a class rep 'Course rep' or 'Not Course Rep' ");
+    io::stdin().read_line(&mut input5).expect("error");
+    let are_you_a_class_rep:String = input5.trim().parse().expect("invalid");
 
-    if class_rep = "yes"
 
-    println!("Are you in 100 level?: ");
-    io::stdin().read_line(&mut input6)expect("error");
-    let Are you in 100 level:String = input6.trim().parse().expect("invalid");
+    println!("What level are you in?: ");
+    io::stdin().read_line(&mut input6).expect("error");
+    let are_you_are_level:f32 = input6.trim().parse().expect("invalid");
 
-    if department = "no"
 
     println!("Whats your CGPA?");
-    io::stdin().read_line(&mut input7)expect("error");
-    let CGPA:String = input7.trim().parse().expect("invalid");
+    io::stdin().read_line(&mut input7).expect("error");
+    let cgpa:f64 = input7.trim().parse().expect("invalid");
 
-    if CGPA = >4.0
 
-    println!("You can vote");
 
+//Validation of the eligibility to vote
+
+if cgpa >= 4.0 && are_you_a_class_rep == "Course Rep" && are_you_are_level >= 100.0 {
+println!("You Are Qualified to vote");
+println!("{}",name);
+println!("{}",email);
+println!("{}",department);
+println!("{}",state_of_origin);
+
+} else{
+    println!("You are not qualified to vote");
+
+
+
+
+
+ }
 
 
 
---
+++ week-6/project-2/src/main.rs
@@ -0,0 +1,46 @@
+//Rust Program To Show The Multiplication
+
+use std::io;
+fn main() {
+    for x in 1..13{
+    println!("\nEnter The Number");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Invalid Input");
+    let n:i64 = input.trim().parse().expect("Invalid Input");
+
+
+    Let::multi1::i64 = n * 1;
+    Let::multi2::i64 = n * 2;
+    Let::multi3::i64 = n * 3;
+    Let::multi4::i64 = n * 4;
+    Let::multi5::i64 = n * 5;
+    Let::multi6::i64 = n * 6;
+    Let::multi7::i64 = n * 7;
+    Let::multi8::i64 = n * 8;
+    Let::multi9::i64 = n * 9;
+    Let::multi10::i64 = n * 10;
+    Let::multi11::i64 = n * 11;
+    Let::multi12::i64 = n * 12;
+
+
+
+
+
+    println!("The Multiplication of 1 to n");
+    println!("{}",multi1);
+    println!("{}",multi2);
+    println!("{}",multi3);
+    println!("{}",multi4);
+    println!("{}",multi5);
+    println!("{}",multi6);
+    println!("{}",multi7);
+    println!("{}",multi8);
+    println!("{}",multi9);
+    println!("{}",multi10);
+    println!("{}",multi11);
+    println!("{}",multi12);
+
+
+}
+}
+ 
